27ccbc5c8988295402a8c19f1790807076b3eb4f
ezequieljsosa <ezequieljsosa@gmail.com>
2023-03-20 09:22:27 -0300

    Update TestAlgo.java

--- src/test/java/ar/edu/utn/ap/clase7/TestAlgo.java
+++ src/test/java/ar/edu/utn/ap/clase7/TestAlgo.java
@@ -3,6 +3,9 @@ package ar.edu.utn.ap.clase7;
 public class TestAlgo {
 
 	public void untestX() {
+		System.out.println("hola2");
+	} 
+	public void untestY() {
 		System.out.println("hola");
 	} 
 }
